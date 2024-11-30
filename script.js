c1a745b6b53d5002fcfd69fb6f3297eec8ab6071
347Johann <167218863+347Johann@users.noreply.github.com>
2024-11-30 00:04:25 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// DOM Elements
+uppercaseCheckbox = document.getElementById("Uppercase")
+lowercaseCheckbox = document.getElementById("Lowercase")
+numberCheckbox = document.getElementById("Numbers")
+symbolCheckbox = document.getElementById("Symbols")
+passwordLength = document.getElementById("Password-Length")
+passwordOutput = document.getElementById("Password-Output")
+
+// Variables
+const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const symbols = '!@#$%^&*()_+[]{}|;:,.<>?';
+
+function GeneratePassword(){
+  passwordOutput.style.color = "black";
+  length = parseInt(passwordLength.value)
+  wordSelection = ""
+  password = ""
+
+  if (!uppercaseCheckbox.checked && !lowercaseCheckbox.checked && 
+  !numberCheckbox.checked && !symbolCheckbox.checked){
+    passwordOutput.value = "Pick Atleast One Option"
+    passwordOutput.style.color = "red"
+    return
+  }
+  if (length <= 0 || isNaN(length) || length > 1000){
+    passwordOutput.value = "Invalid Password Length"
+    passwordOutput.style.color = "red"
+    return
+  }
+  if (uppercaseCheckbox.checked) wordSelection += uppercase;
+  if (lowercaseCheckbox.checked) wordSelection += lowercase;
+  if (numberCheckbox.checked) wordSelection += numbers;
+  if (symbolCheckbox.checked) wordSelection += symbols;
+
+  for (i = 0; i < length; i++){
+    password += wordSelection[Math.floor(Math.random() * wordSelection.length)]
+  }
+
+  passwordOutput.value = password
+}
